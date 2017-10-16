8215c79fbe724840ff590a72a85a4f19f4f03cac
Daniel Lirio Reis <daniellirioreis@gmail.com>
2017-10-16 17:17:03 -0200

    Tipo de notificação prestações de contas

--- app/enumerations/type_message.rb
+++ app/enumerations/type_message.rb
@@ -1,6 +1,7 @@
 class TypeMessage < EnumerateIt::Base
   associate_values :notification                         => 0,
                    :warning                              => 1,
-                   :convocation                          => 2
+                   :convocation                          => 2,
+                   :accountability                       => 3
                    
 end
